e8e287d1b443eae7a5aeb47298571c2faa0c6be7
Jason Prendergast <32454226+JasonPrendergast@users.noreply.github.com>
2017-10-03 12:34:46 +0100

    Update ScratchKMeans.py

--- ScratchKMeans.py
+++ ScratchKMeans.py
@@ -72,29 +72,3 @@ class K_Means:
         classification = distances.index(min(distances))
         return classification
 
-#clf = K_Means()
-#clf.fit(X)
-
-#for centroid in clf.centroids:
-#    plt.scatter(clf.centroids[centroid][0], clf.centroids[centroid][1],
-#                marker="o", color="k", s=150, linewidths=5)
-#
-#for classification in clf.classifications:
-#    color = colors[classification]
-#    for featureset in clf.classifications[classification]:
-#       plt.scatter(featureset[0], featureset[1], marker="x", color=color, s=150, linewidths=5)
-
-#unknowns = np.array([[1,3],
-#                     [8,9],
-#                     [0,3],
-#                     [5,4],
-#                     [6,4],])
-#
-#for unknown in unknowns:
-#    classification = clf.predict(unknown)
-#    plt.scatter(unknown[0], unknown[1], marker="*", color=colors[classification], s=150, linewidths=5)
-
-
-
-
-#plt.show()
